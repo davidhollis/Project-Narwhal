1a5e1b909731828064c81b934d87fb91abea28ad
janders <janderson3@gatech.edu>
2010-03-12 17:48:48 -0500

    Added tower subclasses

---
+++ src/org/etotheipi/narwhal/domain/tower/LoveTower.java
@@ -0,0 +1,13 @@
+package org.etotheipi.narwhal.domain.tower;
+
+import org.etotheipi.narwhal.domain.Tower;
+
+public class LoveTower extends Tower {
+
+	@Override
+	public void attack() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/org/etotheipi/narwhal/domain/tower/SunshineTower.java
@@ -0,0 +1,13 @@
+package org.etotheipi.narwhal.domain.tower;
+
+import org.etotheipi.narwhal.domain.Tower;
+
+public class SunshineTower extends Tower {
+
+	@Override
+	public void attack() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
