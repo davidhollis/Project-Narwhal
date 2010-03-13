d8b3c3d0e7d37ee718bb285da5ddff0b9a8f9312
janders <janderson3@gatech.edu>
2010-03-13 07:59:35 -0500

    Rainbow towers shoot

--- src/org/etotheipi/narwhal/domain/tower/RainbowTower.java
+++ src/org/etotheipi/narwhal/domain/tower/RainbowTower.java
@@ -15,7 +15,7 @@ public class RainbowTower extends Tower {
 	protected int[][] getStats() {
 		return new int[][] {
 				{10, 15, 20, 25, 100}, // Power
-				{0,  0,  0,  0,  0},   // Range
+				{1,  1,  1,  1,  1},   // Range
 				{0,  0,  0,  0,  0},   // Rate of Fire
 				{30, 40, 50, 60, 150}  // Cost
 		};
