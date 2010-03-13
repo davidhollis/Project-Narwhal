3cd26523972cecb312ef924c9e99e035644d3931
bobby <jking31cs@gmail.com>
2010-03-13 00:41:13 -0500

    Added Game Background to Constants

--- src/org/etotheipi/narwhal/Constants.java
+++ src/org/etotheipi/narwhal/Constants.java
@@ -15,12 +15,14 @@ public class Constants {
 	public static final URL WISH_TOWER = NarwhalMain.class.getResource("art/StarTower.png");
 	public static final URL UNICORN_TOWER = NarwhalMain.class.getResource("art/UnicornTower.png");
 	public static final URL SUNSHINE_TOWER = NarwhalMain.class.getResource("art/SunTower.png");
+	public static final URL GAME_BACKGROUND = NarwhalMain.class.getResource("art/GameBackground.png");
 
 	public static final ImageIcon LOVE_TOWER_ICON = new ImageIcon(LOVE_TOWER);
 	public static final ImageIcon RAINBOW_TOWER_ICON = new ImageIcon(RAINBOW_TOWER);
 	public static final ImageIcon WISH_TOWER_ICON = new ImageIcon(WISH_TOWER);
 	public static final ImageIcon UNICORN_TOWER_ICON = new ImageIcon(UNICORN_TOWER);
 	public static final ImageIcon SUNSHINE_TOWER_ICON = new ImageIcon(SUNSHINE_TOWER);
+	public static final ImageIcon GAME_BACKGROUND_IMAGE = new ImageIcon(GAME_BACKGROUND);
 
 	public static final int MAX_TOWER_LEVEL = 5;
 }
