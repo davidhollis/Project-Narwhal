f7ff00919a7ffe192df1528475834a3d189078f4
bobby <jking31cs@gmail.com>
2010-03-12 19:48:52 -0500

    Set up Constants file to store often used variables.

---
+++ src/org/etotheipi/narwhal/Constants.java
@@ -0,0 +1,24 @@
+package org.etotheipi.narwhal;
+
+import javax.swing.ImageIcon;
+
+/**
+ * Sets up overly used constants so that we can use them all over the
+ * application.  Used for file paths and icons.
+ */
+public class Constants {
+
+	public static final String HEART_TOWER = "HeartTower.png";
+	public static final String RAINBOW_TOWER = "RainbowTower.png";
+	public static final String WISH_TOWER = "WishTower.png";
+	public static final String UNICORN_TOWER = "UnicornTower.png";
+	public static final String SUNSHINE_TOWER = "SunshineTower.png";
+	
+	public static final ImageIcon HEART_TOWER_ICON = new ImageIcon(HEART_TOWER);
+	public static final ImageIcon RAINBOW_TOWER_ICON = new ImageIcon(RAINBOW_TOWER);
+	public static final ImageIcon WISH_TOWER_ICON = new ImageIcon(WISH_TOWER);
+	public static final ImageIcon UNICORN_TOWER_ICON = new ImageIcon(UNICORN_TOWER);
+	public static final ImageIcon SUNSHINE_TOWER_ICON = new ImageIcon(SUNSHINE_TOWER);
+	
+	
+}
